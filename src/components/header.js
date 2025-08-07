06c8f6bc2b912747c206240d2939e6e81a7504c0
unknown <akanofunmilayo44@gmail.com>
2025-08-07 15:20:59 +0100

    updating style

--- src/components/header.js
+++ src/components/header.js
@@ -49,8 +49,8 @@ const Header = () => {
                 <div className="icons">
                      <Link to="/" className="Link"><i class="bi bi-balloon-heart" ></i></Link> 
                      <div className="cart-link">
-                        {isAuthenticated ? (<><Link to="/cart" className="Link"> <i class="bi bi-cart"></i></Link>
-                            <span>{cart.length}</span>  </>):
+                        {isAuthenticated ? (<><Link to="/cart" className="Link cart-icon-with-badge"> <i class="bi bi-cart"></i></Link>
+                            <span className=".cart-badge">{cart.length}</span>  </>):
                         (<i class="bi bi-cart" onClick={() => loginWithRedirect()}></i>)}
                      </div>       
                 </div>
